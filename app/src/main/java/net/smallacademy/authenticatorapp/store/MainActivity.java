af0834842c849dc9b71e7c1261290c9947f21ed8
Akshay Kanchan <akshaykanchan249@gmail.com>
2022-04-25 22:00:56 +0530

    first commit

---
+++ app/src/main/java/net/smallacademy/authenticatorapp/store/MainActivity.java
@@ -0,0 +1,16 @@
+package net.smallacademy.authenticatorapp.store;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import net.smallacademy.authenticatorapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
